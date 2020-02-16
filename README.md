2c25cfb006ba436892cadc570b1e6894e02ac496
Jenny Linh Nguyen <lnguyen@colgate.edu>
2020-02-16 20:38:38 +0000

    Add Optional Features for Lab 1

--- README.md
+++ README.md
@@ -7,8 +7,14 @@ Coursework for CodePath, Mobile App Design Course
 - [x] Adding customized background and text color 
 - [x] Toggle between question and answer side
 - [x] Style the card with drawable 
-
 ###### App Walk-through 
 <img src="https://i.imgur.com/zFcFjDW.gif" width=200><br>
 
+
+#### Optional Features
+- [x] Add selectable multiple choice answers beneath the card
+- [x] Change the background color of the multiple choice answers when clicked to indicate whether the question was answered correctly
+- [x] Add a button that will toggle hiding and showing the answer choices
+###### App Walk-through 
+<img src="https://imgur.com/CqWimxA.gif" width=200><br>
 
--- app/src/main/java/com/project/jennys/flashcard/MainActivity.java
+++ app/src/main/java/com/project/jennys/flashcard/MainActivity.java
@@ -3,6 +3,7 @@ package com.project.jennys.flashcard;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,25 +12,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.correct_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.correct_answer).setBackgroundColor(
+                        getResources().getColor(R.color.green)
+                );
             }
         });
 
-        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.wrong_answer1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.wrong_answer1).setBackgroundColor(
+                        getResources().getColor(R.color.red)
+                );
+                findViewById(R.id.correct_answer).setBackgroundColor(
+                        getResources().getColor(R.color.green)
+                );
+            }
+        });
+
+        findViewById(R.id.wrong_answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.wrong_answer2).setBackgroundColor(
+                        getResources().getColor(R.color.red)
+                );
+
+                findViewById(R.id.correct_answer).setBackgroundColor(
+                        getResources().getColor(R.color.green)
+                );
             }
         });
 
 
+        findViewById(R.id.toogle_choices_visibility).setOnClickListener(new View.OnClickListener() {
 
+            boolean isShowingAnswer = true;
 
+            @Override
+            public void onClick(View v) {
+                if (isShowingAnswer) {
+                    ((ImageView) findViewById(R.id.toogle_choices_visibility)).setImageResource(R.drawable.hide_icon);
+                    findViewById(R.id.correct_answer).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.wrong_answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.wrong_answer2).setVisibility(View.INVISIBLE);
 
+                    isShowingAnswer = false;
+
+                } else {
+                    ((ImageView) findViewById(R.id.toogle_choices_visibility)).setImageResource(R.drawable.show_icon);
+                    findViewById(R.id.correct_answer).setVisibility(View.VISIBLE);
+                    findViewById(R.id.wrong_answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.wrong_answer2).setVisibility(View.VISIBLE);
+
+                    isShowingAnswer = true;
+                }
+
+            }
+        });
     }
 }
