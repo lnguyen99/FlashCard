e4d2e1c8c1cccb2830e4db869198a16a670c4e8b
Jenny Linh Nguyen <lnguyen@colgate.edu>
2020-02-16 19:52:03 +0000

    First part

---
+++ README.md
@@ -0,0 +1,11 @@
+#Flash Card App 
+
+Coursework for CodePath, Mobile App Design Course 
+
+####Part 1: 
+- [x] Create flippable question and answer (hard-coded content)
+- [x] Adding customized background and text color 
+- [x] Toggle between question and answer side
+- [x] Style the card with drawable 
+
+[Imgur](https://imgur.com/zFcFjDW)
--- app/src/main/java/com/project/jennys/flashcard/MainActivity.java
+++ app/src/main/java/com/project/jennys/flashcard/MainActivity.java
@@ -1,7 +1,7 @@
 package com.project.jennys.flashcard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,12 +11,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.mainView).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcardQuestion).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcardAnswer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
             }
         });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
+
+
     }
 }
